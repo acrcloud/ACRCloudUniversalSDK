5f5b69ab20dab7ca059a49b45700facd2f8c76ed
pony <383385741@qq.com>
2021-09-10 16:57:03 +0800

    update offline db demo project.

---
+++ offline_version/ACRCloudSDKDemo/app/src/main/java/com/acr/rec/acrcloudsdkdemo/MainActivity.java
@@ -0,0 +1,262 @@
+package com.acr.rec.acrcloudsdkdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.acrcloud.rec.sdk.ACRCloudClient;
+import com.acrcloud.rec.sdk.ACRCloudConfig;
+import com.acrcloud.rec.sdk.IACRCloudListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+
+
+public class MainActivity extends AppCompatActivity implements IACRCloudListener {
+
+    private ACRCloudClient mClient;
+    private ACRCloudConfig mConfig;
+
+    private TextView mVolume, mResult, tv_time;
+
+    private boolean mProcessing = false;
+    private boolean initState = false;
+
+    private String path = "";
+
+    private long startTime = 0;
+    private long stopTime = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.verifyPermissions();
+
+        path = Environment.getExternalStorageDirectory().toString()
+                + "/acrcloud/model";
+
+        File file = new File(path);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        mVolume = (TextView) findViewById(R.id.volume);
+        mResult = (TextView) findViewById(R.id.result);
+        tv_time = (TextView) findViewById(R.id.time);
+
+        Button startBtn = (Button) findViewById(R.id.start);
+        startBtn.setText(getResources().getString(R.string.start));
+
+        Button stopBtn = (Button) findViewById(R.id.stop);
+        stopBtn.setText(getResources().getString(R.string.stop));
+
+        findViewById(R.id.stop).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        stop();
+                    }
+                });
+
+        Button cancelBtn = (Button) findViewById(R.id.cancel);
+        cancelBtn.setText(getResources().getString(R.string.cancel));
+
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                start();
+            }
+        });
+
+        findViewById(R.id.cancel).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        cancel();
+                    }
+                });
+
+
+        this.mConfig = new ACRCloudConfig();
+        this.mConfig.acrcloudListener = this;
+
+        // If you implement IACRCloudResultWithAudioListener and override "onResult(ACRCloudResult result)", you can get the Audio data.
+        //this.mConfig.acrcloudResultWithAudioListener = this;
+
+        this.mConfig.context = this;
+        this.mConfig.host = "XXXXXXXX";
+        this.mConfig.dbPath = path; // offline db path, you can change it with othe1..
+        // r path which this app can access.
+        this.mConfig.accessKey = "XXXXXXXX";
+        this.mConfig.accessSecret = "XXXXXXXX";
+        this.mConfig.protocol = ACRCloudConfig.ACRCloudNetworkProtocol.PROTOCOL_HTTPS; // PROTOCOL_HTTP
+        this.mConfig.reqMode = ACRCloudConfig.ACRCloudRecMode.REC_MODE_REMOTE;
+        //this.mConfig.reqMode = ACRCloudConfig.ACRCloudRecMode.REC_MODE_LOCAL;
+        //this.mConfig.reqMode = ACRCloudConfig.ACRCloudRecMode.REC_MODE_BOTH;
+
+        this.mClient = new ACRCloudClient();
+        // If reqMode is REC_MODE_LOCAL or REC_MODE_BOTH,
+        // the function initWithConfig is used to load offline db, and it may cost long time.
+        this.initState = this.mClient.initWithConfig(this.mConfig);
+        if (this.initState) {
+//            this.mClient.startPreRecord(3000); //start prerecord, you can call "this.mClient.stopPreRecord()" to stop prerecord.
+        }
+    }
+
+    public void start() {
+        if (!this.initState) {
+            Toast.makeText(this, "init error", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!mProcessing) {
+            mProcessing = true;
+            mVolume.setText("");
+            mResult.setText("");
+            if (this.mClient == null || !this.mClient.startRecognize()) {
+                mProcessing = false;
+                mResult.setText("start error!");
+            }
+            startTime = System.currentTimeMillis();
+        }
+    }
+
+    protected void stop() {
+        if (mProcessing && this.mClient != null) {
+            this.mClient.stopRecordToRecognize();
+        }
+        mProcessing = false;
+
+        stopTime = System.currentTimeMillis();
+    }
+
+    protected void cancel() {
+        if (mProcessing && this.mClient != null) {
+            mProcessing = false;
+            this.mClient.cancel();
+            tv_time.setText("");
+            mResult.setText("");
+        }
+    }
+
+    // Old api
+    @Override
+    public void onResult(String result) {
+        if (this.mClient != null) {
+            this.mClient.cancel();
+            mProcessing = false;
+        }
+
+        String tres = "\n";
+
+        try {
+            JSONObject j = new JSONObject(result);
+            JSONObject j1 = j.getJSONObject("status");
+            int j2 = j1.getInt("code");
+            if(j2 == 0){
+                JSONObject metadata = j.getJSONObject("metadata");
+                //
+                if (metadata.has("humming")) {
+                    JSONArray hummings = metadata.getJSONArray("humming");
+                    for(int i=0; i<hummings.length(); i++) {
+                        JSONObject tt = (JSONObject) hummings.get(i);
+                        String title = tt.getString("title");
+                        JSONArray artistt = tt.getJSONArray("artists");
+                        JSONObject art = (JSONObject) artistt.get(0);
+                        String artist = art.getString("name");
+                        tres = tres + (i+1) + ".  " + title + "\n";
+                    }
+                }
+                if (metadata.has("music")) {
+                    JSONArray musics = metadata.getJSONArray("music");
+                    for(int i=0; i<musics.length(); i++) {
+                        JSONObject tt = (JSONObject) musics.get(i);
+                        String title = tt.getString("title");
+                        JSONArray artistt = tt.getJSONArray("artists");
+                        JSONObject art = (JSONObject) artistt.get(0);
+                        String artist = art.getString("name");
+                        tres = tres + (i+1) + ".  Title: " + title + "    Artist: " + artist + "\n";
+                    }
+                }
+                if (metadata.has("streams")) {
+                    JSONArray musics = metadata.getJSONArray("streams");
+                    for(int i=0; i<musics.length(); i++) {
+                        JSONObject tt = (JSONObject) musics.get(i);
+                        String title = tt.getString("title");
+                        String channelId = tt.getString("channel_id");
+                        tres = tres + (i+1) + ".  Title: " + title + "    Channel Id: " + channelId + "\n";
+                    }
+                }
+                if (metadata.has("custom_files")) {
+                    JSONArray musics = metadata.getJSONArray("custom_files");
+                    for(int i=0; i<musics.length(); i++) {
+                        JSONObject tt = (JSONObject) musics.get(i);
+                        String title = tt.getString("title");
+                        tres = tres + (i+1) + ".  Title: " + title + "\n";
+                    }
+                }
+                tres = tres + "\n\n" + result;
+            }else{
+                tres = result;
+            }
+        } catch (JSONException e) {
+            tres = result;
+            e.printStackTrace();
+        }
+
+        mResult.setText(tres);
+    }
+
+    @Override
+    public void onVolumeChanged(double volume) {
+        long time = (System.currentTimeMillis() - startTime) / 1000;
+        mVolume.setText(getResources().getString(R.string.volume) + volume + "\n\nRecord Time: " + time + " s");
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+    };
+    public void verifyPermissions() {
+        for (int i=0; i<PERMISSIONS.length; i++) {
+            int permission = ActivityCompat.checkSelfPermission(this, PERMISSIONS[i]);
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS,
+                        REQUEST_EXTERNAL_STORAGE);
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MainActivity", "release");
+        if (this.mClient != null) {
+            this.mClient.release();
+            this.initState = false;
+            this.mClient = null;
+        }
+    }
+}
